7fab7dc7fdd3f87726634b1b3f8bdfe2a6563ea4
kwongiho <shng10503@naver.com>
2017-03-22 16:14:57 +0900

    Initial Commit

---
+++ algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithm
+//
+//  Created by Kwongiho on 2017. 3. 22..
+//  Copyright © 2017년 Kwongiho. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
