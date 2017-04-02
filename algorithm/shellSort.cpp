745bb43a5b82255095770056670c19ca74a7407e
kwongiho <shng10503@naver.com>
2017-04-02 22:57:29 +0900

    modified comment

--- algorithm/shellSort.cpp
+++ algorithm/shellSort.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 #define SWAP(a,b)  {int t; t = a; a=b; b=t;}
-
-//global
+// i am was used refer by Mr. 장문석 teacher.
+//Just will using only for watching about process
 int *origin;
 int arraySize ;
 
@@ -20,7 +20,7 @@ void shellSort(int *base, int size)
 {
     int i, step;
 
-    for (step = size / 2; step>0; step /= 2)//step의 폭을 1/2로 줄여간다.
+    for (step = size / 2; step>0; step /= 2)// using N/2
     {
         printf("step:%d\n", step);
         for (i = 0; i<step; i++) //step 간격에 있는 요소들을 삽입정렬
@@ -30,9 +30,6 @@ void shellSort(int *base, int size)
 void insertionSort2(int *base, int size, int step)
 {
     int i, j;
-    /**
-     *
-     */
     for (i = step; i<size; i += step)//정렬 대상 원소는 step 간격으로 이동
         for (j = i; j>0; j -= step)//step 간격으로 앞으로 이동
             if (base[j - step]>base[j])//앞쪽이 더 클 때
