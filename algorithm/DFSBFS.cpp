e1ebb68074ca10e78b318d29eeb7aac9b179e2df
kwongiho <shng10503@naver.com>
2017-04-13 02:49:02 +0900

    add dfsbfs

---
+++ algorithm/DFSBFS.cpp
@@ -0,0 +1,49 @@
+#include<stdio.h>
+bool t[1001][1001];
+bool t_v[1001];//dfs에서 사용할 visit
+bool t_vv[1001];//bfs에서 사용할 visit
+int que[1001];
+int n, m, v;
+int front = 0;
+int rear = 0;
+int b=0;
+void dfs(int);
+void bfs(int);
+
+int main(){
+    int a,b;
+    scanf(" %d%d%d",&n,&m,&v);
+    for (int i = 0; i <m ; i++){
+        scanf(" %d%d",&a,&b);
+        t[b][a]=t[a][b] = true;
+    }
+
+    dfs(v);
+    printf("\n");
+
+    bfs(v);
+    printf("\n");
+
+}
+void dfs(int a){
+    t_v[a]=true;
+    printf("%d ",a);
+    for (int i = 1; i <= 1000; i++)
+        if (!t_v[i] && t[a][i])
+            dfs(i);
+}
+void bfs(int a){
+    t_vv[a] = true;
+    que[rear] = a;
+    rear++;
+    while (front != rear){
+        b=que[front++];
+        printf("%d ",b);
+        for (int i = 1; i <=1000 ; i++)
+            if (!t_vv[i] && t[b][i]){
+                que[rear++] = i;
+                t_vv[i] = true;
+            }
+    }
+}
+
--- algorithm/dfs.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  dfs.cpp
-//  algorithm
-//
-//  Created by Kwongiho on 2017. 3. 22..
-//  Copyright © 2017년 Kwongiho. All rights reserved.
-//
-
-#include "dfs.hpp"
